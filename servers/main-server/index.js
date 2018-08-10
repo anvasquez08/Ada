98de1cde88e8fb2505ee497a632a9593717f7a29
DDK <ddkgeorgetown@gmail.com>
2018-08-10 14:31:43 -0400

    Seeding inventory db with sample items from asos

--- servers/main-server/index.js
+++ servers/main-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const db = require('../../databases/Inventory')
 
 const app = express()
 app.use(morgan('dev'))
