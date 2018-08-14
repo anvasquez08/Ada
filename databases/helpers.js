4578873ef5369dd690fb938a5833c277d0efd2cd
Ana Vasquez <anvasquez08@gmail.com>
2018-08-14 11:52:28 -0400

    Code cleanup and file restructure.

--- databases/helpers.js
+++ databases/helpers.js
@@ -1,4 +1,4 @@
-const { Item } = require("./schema.js");
+const { Item, ItemKeywords, Timestamp } = require("./schema.js");
 
 exports.saveItem = (id, name, brandName, url, imageUrl, price) => {
   new Item({
@@ -13,3 +13,74 @@ exports.saveItem = (id, name, brandName, url, imageUrl, price) => {
     .then(response => console.log("Successfully saved data"))
     .catch(err => console.log("Error in database save function"));
 };
+
+exports.indexItem = (id, itemLabels) => {
+
+  async.each(itemLabels, (label) => {
+    Item.findOne({keyword: label}, (err, keywordItem) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (keywordItem === null) {
+          let itemToSave = new Item({
+            keyword: label,
+            inventoryIds: [id]
+          })
+          itemToSave.save((err) => {
+            if (err) {
+              console.log(err);
+            }
+          })
+        } else {
+          Item.findOneAndUpdate({keyword: label}, {$push: {inventoryIds: id}}, (err) => {
+            if (err) {
+              console.log('ERR UPDATING KEYWORD')
+            }
+          })
+        }
+      }
+    })
+  }, (err) => {
+    console.log(err);
+  })
+}
+
+exports.getKetwordEntries = (itemKeywords, callback) => {
+  Item.find({keyword: {$in: itemKeywords}}, (err, results) => {
+    if (err) {
+      callback(err);
+    } else {
+      console.log('RESULTS', results);
+      callback(null, results);
+    }
+  })
+}
+
+exports.updateRecentTimestamp = (timestamp) => {
+  Timestamp.findOneAndUpdate({}, {timestamp: timestamp}, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  })
+}
+
+exports.getRecentTimestamp = (callback) => {
+  Timestamp.findOne({}, (err, latestTimestamp) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, latestTimestamp);
+    }
+  })
+}
+
+
+exports.retrieveNewItems = (timestamp, callback) => {
+    Item.find({timestamp: {$gte: timestamp}}, (err, newItems) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, newItems);
+      }
+    })
+  }
--- servers/main-server/index.js
+++ servers/main-server/index.js
@@ -1,79 +1,77 @@
-const express = require("express");
-const graph = require("express-graphql");
-const morgan = require("morgan");
+// const express = require("express");
+// const graph = require("express-graphql");
+// const morgan = require("morgan");
+// const passport = require('passport')
+// const cors = require('cors')
+// const bodyParser = require("body-parser");
+// const fileUpload = require('express-fileupload');
 
-const passport = require('passport')
-const db = require('../../databases/Inventory')
-const cors = require('cors')
-const bodyParser = require("body-parser");
-const recommendationService = require('./recommendations/service/imageTraits.js');
-const scraper = require('./services/scraper')
-const authRouter = require('../routes/authRoutes')
-const recWorker = require('./recommendations/worker/recommendationWorker.js')
-const fileUpload = require('express-fileupload');
-const imageUpload = require('./imageUpload/uploadToBucket.js');
-const { inventoryDB } = require('../../databases/index.js')
+// const scraper = require('./services/scraper')
+// const authRouter = require('../routes/authRoutes')
+// const recWorker = require('./recommendations/worker/recommendationWorker.js')
+// const imageUpload = require('./imageUpload/uploadToBucket.js');
+// const recommendationService = require('./recommendations/service/imageTraits.js');
+// const { inventoryDB, imageDB } = require('../../databases/index.js')
 
+// const app = express();
+// app.use(fileUpload());
+// app.use(cors())
+// app.use(morgan("dev"));
+// app.use(bodyParser.json());
+// app.use(express.static(__dirname + "/../../client/dist"));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use('/auth', authRouter)
 
-const app = express();
-app.use(fileUpload());
-app.use(cors())
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(express.static(__dirname + "/../../client/dist"));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use('/auth', authRouter)
+// /*============== Graph QL ============== */
+// const gqlSchema = require('../../databases/gqlSchema.js');
+// app.use("/graphql", bodyParser.json(), graph({ schema: gqlSchema,  graphiql: true  }));
 
-/*============== Graph QL ============== */
-const gqlSchema = require('../../databases/gqlSchema.js');
-app.use("/graphql", bodyParser.json(), graph({ schema: gqlSchema,  graphiql: true  }));
+// /*====================================== */
 
-/*====================================== */
+// app.get('/scrape', scraper.googleScrape)
+// app.get('/tags', scraper.getByTags)
 
-app.get('/scrape', scraper.googleScrape)
-app.get('/tags', scraper.getByTags)
-
-app.post('/index', function(req, res) {
-    let url = 'http://greenwoodhypno.co.uk/wp-content/uploads/2014/09/test-image.png'
-    let testID = 999;
-    recWorker.indexAnalyzeInventoryItem(testID, url, (err) => {
-        if (err) {
-            res.send(err)
-        } else {
-            res.send('success');
-        }
-    });
+// app.post('/index', function(req, res) {
+//     let url = 'http://greenwoodhypno.co.uk/wp-content/uploads/2014/09/test-image.png'
+//     let testID = 999;
+//     recWorker.indexAnalyzeInventoryItem(testID, url, (err) => {
+//         if (err) {
+//             res.send(err)
+//         } else {
+//             res.send('success');
+//         }
+//     });
     
-});
+// });
 
-app.post('/recommend', function(req, res) {
-    let url = 'http://greenwoodhypno.co.uk/wp-content/uploads/2014/09/test-image.png'
-    recommendationService.getRecommendationsForURL(url, (err, recommendations) => {
-        if (err) {
-            res.send(err)
-        } else {
-            res.send(recommendations);
-        }
-    });
+// app.post('/recommend', function(req, res) {
+//     let url = 'https://coding-jacks-awesome-bucket.s3.us-west-2.amazonaws.com/018993_BPI_KIDS_OWL_KIDS_HAT_AW15_3_l.jpg'
+//     recommendationService.getRecommendationsForURL(url, (err, recommendations) => {
+//         if (err) {
+//             res.send(err)
+//         } else {
+//             res.send(recommendations);
+//         }
+//     });
     
-});
+// });
 
-app.post('/upload', (req,res) => {
-    let imageFile = req.files.file;
-    imageUpload.uploadImage(imageFile, (err, fileURL) => {
-        if (err) {
-            console.log(err);
-            res.status(400).send(err);
-        } else {
-            console.log(fileURL)
-        }
-    })
+// app.post('/upload', (req,res) => {
+//     let imageFile = req.files.file;
+//     imageUpload.uploadImage(imageFile, (err, fileURL) => {
+//         if (err) {
+//             console.log(err);
+//             res.status(400).send(err);
+//         } else {
+//             console.log(fileURL)
+//         }
+//     })
     
-  })
+//   })
 
-app.post('/update', function(req, res) {
-    recWorker.updateIndexDB();
-});
+// app.post('/update', function(req, res) {
+//     recWorker.updateIndexDB();
+// });
 
-app.listen(8080, () => console.log("Listening on port 8080"));
+// app.listen(8080, () => console.log("Listening on port 8080"));
