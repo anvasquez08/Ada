bc5f8e370283f79a7ded0427b1b6aed0c3849244
Jack Lim <jacklim.tkl@gmail.com>
2018-08-25 17:31:46 -0400

    included files

---
+++ client/src/components/svgs/IconFB.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const IconFB = () => (
+  <svg width="16px" height="16px" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg"
+  xmlnsXlink="http://www.w3.org/1999/xlink">
+  <path fill="#000" d="M16.438 28V15.228h4.287l.642-4.978h-4.929V7.072c0-1.44.4-2.423 2.467-2.423h2.636V.195C21.085.136 19.52 0 17.7 0c-3.8 0-6.402 2.32-6.402 6.58v3.67H7v4.978h4.298V28h5.14z"/>
+</svg>
+)
+
+export default IconFB;
