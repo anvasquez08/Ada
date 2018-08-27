0e84c869d3868c024039eea4ec61c013ffb27574
Jon Izak <jonathanizak@gmail.com>
2018-08-27 15:17:41 -0400

    clean up console logs

--- servers/index.js
+++ servers/index.js
@@ -259,19 +259,14 @@ server.express.post('/recommend/:user', function(req, res) {
 });
 
 server.express.post('/recommend/insta', function(req, res) {
-    console.log("Receiving Instagram selected photos: ", req.body.params)
     let aggregateTags = [];
     let instagramPhotos = req.body.params;
     for (var i=0; i<instagramPhotos.length; i++) {
         recommendationService.getRecommendationsForImageUrl(instagramPhotos[i], (err, recommendations) => {
             if (err) {
-                console.log("Error getting recommendations using image URL", err)
                 res.status(500).send();
             } else {
                 aggregateTags.push(recommendations);
-                console.log("Console logging recommendations length: ", recommendations.length)
-                // res.status(200).send(recommendations);
-                console.log("Console logging aggregateTags length", aggregateTags.length )
                 res.status(200).send();
             }
         })
