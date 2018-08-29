26e303ec010cd760c67e4b8b2a696961ff46c2be
Ana Vasquez <anvasquez08@gmail.com>
2018-08-29 17:10:01 -0400

    Commit to pull down.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -13,9 +13,8 @@ class NavBar extends React.Component {
                 borderBottomWidth: this.props.offSetY <= 44 && this.props.currentPage === '/' ? 0 : 1,
                 transition: "background-color 200ms linear"}}>
       
-        <div className="left menu">{this.props.user ? <a className="item">{this.props.user}</a> : null}</div>
-
         <div className="right menu">
+          <div>{this.props.user ? <a className="item">{this.props.user}</a> : null}</div>
           <span className={this.props.currentPage === '/' ? "ui item active" : "item"}>
             <Link className={this.props.offSetY <= 44 && this.props.currentPage === '/' ? "transparent" : "normal"} to='/'>Home</Link></span>
           <span className={this.props.currentPage === 'style' ? "ui item active" : "item"}>
