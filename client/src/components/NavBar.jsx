1d47b7bef36a98c6eb49ebd68a2374480d805e14
Jon Izak <jonathanizak@gmail.com>
2018-08-30 15:45:33 -0400

    navbar styling

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
+
+
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -9,33 +11,34 @@ class NavBar extends React.Component {
   render() {
     return (
       <div className="ui secondary pointing fixed menu" 
-        style={{backgroundColor: this.props.offSetY <= 44 && this.props.currentPage === '/' ? "transparent" : "#fff9f6",
-                borderBottomWidth: this.props.offSetY <= 44 && this.props.currentPage === '/' ? 0 : 1,
+        style={{backgroundColor: this.props.offSetY <= 44 && this.props.location.pathname === '/' ? "normal" : "#fff9f6",
+                borderBottomWidth: this.props.offSetY <= 44 && this.props.location.pathname === '/' ? 0 : 1,
                 transition: "background-color 200ms linear"}}>
       
-        <div className="left menu">{this.props.user ? <a className="item">{this.props.user}</a> : null}</div>
+        {/* <div className="left menu">{this.props.user ? <a className="item">{this.props.user}</a> : null}</div> */}
 
         <div className="right menu">
-          <span className={this.props.currentPage === '/' ? "ui item active" : "item"}>
-            <Link className={this.props.offSetY <= 44 && this.props.currentPage === '/' ? "transparent" : "normal"} to='/'>Home</Link></span>
-          <span className={this.props.currentPage === 'style' ? "ui item active" : "item"}>
-            <Link className={this.props.offSetY <= 44 && this.props.currentPage === '/' ? "transparent" : "normal"} to='/style'>My Style</Link></span>
-          <span className={this.props.currentPage === 'favorites' ? "ui item active" : "item"}>
-            <Link className={this.props.offSetY <= 44 && this.props.currentPage === '/' ? "transparent" : "normal"} to='/favorites'>Wish List</Link></span>
-            <span className={this.props.currentPage === 'favorites' ? "ui item active" : "item"}>
-            <Link className={this.props.offSetY <= 44 && this.props.currentPage === '/' ? "transparent" : "normal"} to='/trending'>Trending Styles</Link></span>
+          <span className={this.props.location.pathname === '/detect' ? "ui item active" : "item"}>
+            <Link className={this.props.offSetY <= 44 && this.props.location.pathname === '/detect' ? "normal" : "normal"} to='/detect'>Search</Link></span>
+          <span className={this.props.location.pathname === '/style' ? "ui item active" : "item"} style={this.props.user ? {display: 'block'} : {display: 'none'}}>
+            <Link className={this.props.offSetY <= 44 && this.props.location.pathname === '/style' ? "normal" : "normal"} to='/style'>My Style</Link></span>
+          <span className={this.props.location.pathname === '/favorites' ? "ui item active" : "item"} style={this.props.user ? {display: 'block'} : {display: 'none'}}>
+            <Link className={this.props.offSetY <= 44 && this.props.location.pathname === '/favorites' ? "normal" : "normal"} to='/favorites'>Wish List</Link></span>
+            <span className={this.props.location.pathname === '/trending' ? "ui item active" : "item"}>
+            <Link className={this.props.offSetY <= 44 && this.props.location.pathname === '/trending' ? "normal" : "normal"} to='/trending'>Trending Styles</Link></span>
           <span className="item">
             {this.props.user 
-            ? <a className={this.props.offSetY <= 44 && this.props.currentPage === '/' ? "transparent" : "normal"} href="/auth/logout">Logout</a>
-            : <a className={this.props.offSetY <= 44 && this.props.currentPage === '/' ? "transparent" : "normal"} onClick={this.props.toggleModal}>Sign in</a>}
+            ? <a className={this.props.offSetY <= 44 && this.props.currentPage === '/' ? "normal" : "normal"} href="/auth/logout">Logout</a>
+            : <a className={this.props.offSetY <= 44 && this.props.currentPage === '/' ? "normal" : "normal"} onClick={this.props.toggleModal}>Sign in</a>}
           </span>
+          <span className="item">{this.props.user}</span>
         </div>
       </div>
     )
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
 
 
 // Code it so that this.props.currentPage === 'home', 'style', 'favorites'
